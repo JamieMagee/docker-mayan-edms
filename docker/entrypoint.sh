483210276dd62e58e1fbc1d2d2d585af277b39fb
Jamie Magee <JamieMagee@users.noreply.github.com>
2016-08-21 23:08:58 +0200

    Change initialsetup to performupgrade

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,6 +12,6 @@ until nc -z $POSTGRES_PORT_5432_TCP_ADDR $POSTGRES_PORT_5432_TCP_PORT; do
 done
 
 # Migrate database, create initial admin user
-mayan-edms.py initialsetup
+mayan-edms.py performupgrade
 
 exec "$@"
